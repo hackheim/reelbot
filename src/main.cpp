94268c264cd39445a427729c7c9a5bce387d5ff4
hackheim, diverse <kjernen@hackheim.com>
2016-05-08 18:00:57 +0000

    weird bugfix, hardcoded digital pins for arduino nano

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include "Arduino.h"
 
 String inData;
+const int D7 = 10;
+const int D8 = 11;
+const int D9 = 12;
+const int D10 = 13;
 
 void setup() {
     Serial.begin(9600);
     Serial.println("Power On");
     pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(D7, OUTPUT);
+    pinMode(D8, OUTPUT);
     pinMode(D9, OUTPUT);
     pinMode(D10, OUTPUT);
-    pinMode(D11, OUTPUT);
-    pinMode(D12, OUTPUT);
 }
 
 void loop()
@@ -17,7 +21,7 @@ void loop()
     while (Serial.available() > 0)
     {
         char recieved = Serial.read();
-        inData += recieved; 
+        inData += recieved;
 
         // Process message when new line character is recieved
         if (recieved == '\n'){
@@ -31,36 +35,36 @@ void loop()
                 Serial.println("Motor 1 -> Offline");
                 digitalWrite(LED_BUILTIN, LOW);
             }
-//LEDS 9 10 11 12
+//LEDS 7 8 9 10
            if (inData == "LF\n") {
                 Serial.println("Left Motor Forward");
-                digitalWrite(D9, HIGH);
-                digitalWrite(D10, LOW);
+                digitalWrite(D7, HIGH);
+                digitalWrite(D8, LOW);
             }
             if (inData == "LS\n") {
                 Serial.println("Left Motor Stop");
-                digitalWrite(D9, LOW);
-                digitalWrite(D10, LOW);
+                digitalWrite(D7, LOW);
+                digitalWrite(D8, LOW);
             }
             if (inData == "LB\n") {
                 Serial.println("Left Motor Backward");
-                digitalWrite(D9, LOW);
-                digitalWrite(D10, HIGH);
+                digitalWrite(D7, LOW);
+                digitalWrite(D8, HIGH);
             }
             if (inData == "RF\n") {
                 Serial.println("Right Motor Forward");
-                digitalWrite(D11, HIGH);
-                digitalWrite(D12, LOW);
+                digitalWrite(D9, HIGH);
+                digitalWrite(D10, LOW);
             }
             if (inData == "RS\n") {
                 Serial.println("Right Motor Stop");
-                digitalWrite(D11, LOW);
-                digitalWrite(D12, LOW);
+                digitalWrite(D9, LOW);
+                digitalWrite(D10, LOW);
             }
             if (inData == "RB\n") {
                 Serial.println("Right Motor Backward");
-                digitalWrite(D11, LOW);
-                digitalWrite(D12, HIGH);
+                digitalWrite(D9, LOW);
+                digitalWrite(D10, HIGH);
             }
             inData = ""; // Clear recieved buffer
         }
