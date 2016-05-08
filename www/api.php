3ebc0e47866173262342ac8be88f90edc7f20738
hackheim, diverse <kjernen@hackheim.com>
2016-05-08 19:36:30 +0000

    some web syntax bugfix for LF LB etc

--- www/api.php
+++ www/api.php
@@ -1,5 +1,5 @@
 <?php
-$cmd = preg_replace( "/[^LRFBS]/", "", substr($_GET['cmd'], 0, 1));
+$cmd = preg_replace( "/[^LRFBS]/", "", substr($_GET['cmd'], 0, 2));
 
 exec('echo "'.$cmd.'" > /dev/ttyUSB0 >&1', $output);
 
